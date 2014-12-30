1db946fc46c68da9391502cc7440115b09db6a89
yunixon <yunixon@gmail.com>
2014-12-30 13:54:53 +0400

    fix change lang feature spec

--- spec/features/change_lang_spec.rb
+++ spec/features/change_lang_spec.rb
@@ -4,19 +4,19 @@ feature 'Change language' do
 
   scenario 'User change to PL language' do
     visit root_path
-    click_link('pl')
+    click_link('pl') unless I18n.locale.to_s == 'pl'
     expect(page).to have_text 'Zmień język'
   end
 
   scenario 'User change to EN language' do
-    visit root_path
-    click_link('en')
+    visit root_path   
+    click_link('en') unless I18n.locale.to_s == 'en'
     expect(page).to have_text 'Change language'
   end
 
   scenario 'User change to RU language' do
     visit root_path
-    click_link('ru')
+    click_link('ru') unless I18n.locale.to_s == 'ru'
     expect(page).to have_text 'Изменить язык'
   end
 
