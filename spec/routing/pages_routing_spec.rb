6c0960b16b6d53630fbf837f2bf20e708151a833
yunixon <yunixon@gmail.com>
2015-01-04 01:06:38 +0400

    fix warning for pages routing specs

--- spec/routing/pages_routing_spec.rb
+++ spec/routing/pages_routing_spec.rb
@@ -1,15 +1,13 @@
 require 'rails_helper'
 
-describe Page::ApplicationController do
-  describe 'pages routing' do
+describe 'Pages routing' do
     
-    it 'routes /main to the pages controller' do
-      expect(get: '/main').to route_to(controller: 'pages', action: 'showbyshortlink', shortlink: 'main')
-    end
-    
-    it 'routes /not-found to the pages controller' do
-      expect(get: '/not-found').to route_to('pages#not_found')
-    end
-
+  it 'route /main to the pages controller' do
+    expect(get: '/main').to route_to(controller: 'pages', action: 'showbyshortlink', shortlink: 'main')
   end
+    
+  it 'route /not-found to the pages controller' do
+    expect(get: '/not-found').to route_to('pages#not_found')
+  end
+
 end
