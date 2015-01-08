e70ac7247259e04c6fac9153bd374e8e80c4128e
Jerzy Sładkowski <jerzy.sladkowski@gmail.com>
2015-01-08 13:05:39 +0100

    Add deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,58 +1,75 @@
-# config valid only for Capistrano 3.1
-lock '3.2.1'
+require 'capistrano_colors'
+require 'rvm/capistrano'                  # Load RVM's capistrano plugin.
+require 'bundler/capistrano'              # Capistrano task for Bundler.
+require 'cape'
+require 'capistrano/ext/multistage'
+require 'capistrano-unicorn'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :rvm_type, :user
+set :rvm_ruby_string, :release_path
 
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+set :stages, [:staging, :production]
+set :default_stage, :production
 
-# Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+set :rails_env, "staging"
+set :user, "webgate"
+set :runner, user
+set :scm, :git
+set :scm_username, "sysadm"
+set :application, "webgate"
+set :repository, "git@bitbucket.org:#{scm_username}/#{application}.git"
+#ssh_options[:keys] = ["#{ENV['HOME']}/.ssh/id_dsa"]
 
-# Default value for :scm is :git
-# set :scm, :git
+set :keep_releases, 5
+set :use_sudo, false
+set :git_shallow_clone, 1
+set :deploy_via, :remote_cache
+set :shared_children, shared_children + %w(public/uploads)
 
-# Default value for :format is :pretty
-# set :format, :pretty
+default_run_options[:pty] = true
 
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
-
-# Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+before 'deploy:assets:precompile', 'deploy:symlink_shared'
+after 'deploy:update_code', 'deploy:symlink_shared'
+after 'deploy:symlink_shared', 'deploy:migrate'
+after 'deploy', 'deploy:cleanup'
+after 'deploy:assets:force_compile', 'unicorn:restart'
 
 namespace :deploy do
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
-    end
+  task :symlink_shared do
+    run "ln -nsf #{shared_path}/config/database.yml #{release_path}/config/database.yml"
   end
-
-  after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+  namespace :assets do
+    task :force_compile do
+      run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
     end
-  end
 
+    #task :precompile, :roles => :web, :except => { :no_release => true } do
+    #  # Check if assets have changed. If not, don't run the precompile task - it takes a long time.
+    #  force_compile = false
+    #  changed_asset_count = 0
+    #  begin
+    #    from = source.next_revision(current_revision)
+    #    asset_locations = 'app/assets/ lib/assets vendor/assets'
+    #    changed_asset_count = capture("cd #{latest_release} && #{source.local.log(from)} #{asset_locations} | wc -l").to_i
+    #  rescue Exception => e
+    #    logger.info "Error: #{e}, forcing precompile"
+    #    force_compile = false
+    #  end
+    #  if changed_asset_count > 0 || force_compile
+    #    logger.info "#{changed_asset_count} assets have changed. Pre-compiling"
+    #    run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+    #  else
+    #    logger.info "#{changed_asset_count} assets have changed. Skipping asset pre-compilation"
+    #  end
+    #end
+  end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    unicorn.restart
+  end
+  task :start, :roles => :app do
+    unicorn.start
+  end
+  task :stop, :roles => :app do
+    unicorn.stop
+  end
 end
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,45 +1,13 @@
-# Simple Role Syntax
-# ==================
-# Supports bulk-adding hosts to roles, the primary server in each group
-# is considered to be the first unless any hosts have the primary
-# property set.  Don't declare `role :all`, it's a meta role.
+set :domain, "dev.webgate.pro"
 
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+role :web, domain                          # Your HTTP server, Apache/etc
+role :app, domain                          # This may be the same as your `Web` server
+role :db,  domain, :primary => true # This is where Rails migrations will run
 
+server "dev.webgate.pro", :app, :web, :db, :primary => true
 
-# Extended Server Syntax
-# ======================
-# This can be used to drop a more detailed server definition into the
-# server list. The second argument is a, or duck-types, Hash and is
-# used to set extended properties on the server.
-
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
-
-
-# Custom SSH Options
-# ==================
-# You may pass any option but keep in mind that net/ssh understands a
-# limited set of options, consult[net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start).
-#
-# Global options
-# --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
-#
-# And/or per server (overrides global)
-# ------------------------------------
-# server 'example.com',
-#   user: 'user_name',
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: 'user_name', # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: 'please use keys'
-#   }
+set :shared_host, "webgate.pro"
+set :branch, "develop"
+set :unicorn_env, "staging"
+set :deploy_to, "/home/webgate/dev.webgate.pro/"
+require 'capistrano-unicorn'
