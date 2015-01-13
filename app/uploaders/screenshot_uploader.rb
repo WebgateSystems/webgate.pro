4c4528fb765db2c64009ba8b46c87b6af8494e87
Alex Dobr <dobrunya777@gmail.com>
2015-01-13 21:30:42 +0000

    feature air filenameFix

--- app/uploaders/screenshot_uploader.rb
+++ app/uploaders/screenshot_uploader.rb
@@ -12,7 +12,7 @@ class ScreenshotUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{model.class.to_s.underscore}s/project#{model.id}/#{mounted_as}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -84,8 +84,8 @@ class ScreenshotUploader < CarrierWave::Uploader::Base
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  def filename
+      "pic.#{file.extension}" if original_filename
+  end
 
 end
