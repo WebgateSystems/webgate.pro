ced6b8f9f7a213ec886abbfecacad1f7b92da10d
yunixon <yunixon@gmail.com>
2015-01-30 12:22:38 +0400

    home carousel with project screenshots

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,13 @@
 class HomeController < ApplicationController
-  
-  layout 'main'
-  layout 'portfolio', only: :portfolio
+  layout 'main', except: :portfolio
 
   def index
+    @projects = Project.published.all
   end
 
-  def portfolio
+  def portfolio    
     @projects = Project.published.all
+    render layout: 'portfolio'
   end
   
 end
