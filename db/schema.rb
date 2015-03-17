6187e388d7d6ff07ad16418cae73c68a0ac2b53e
Jerzy Sładkowski <jerzy.sladkowski@gmail.com>
2015-03-17 12:34:36 +0100

    New schema

--- db/schema.rb
+++ db/schema.rb
@@ -207,7 +207,6 @@ ActiveRecord::Schema.define(version: 20150311115627) do
     t.string   "locale",        null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "title"
     t.text     "description"
   end
 
