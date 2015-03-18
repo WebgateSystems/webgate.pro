624aa60a6599aa5919b3eacb7eb4e7aec1cb3d6b
Yuri Skurikhin <yunixon@gmail.com>
2015-03-18 07:44:04 +0300

    capybara-webkit

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,9 +5,8 @@ require 'rspec/rails'
 require 'shoulda/matchers'
 require 'capybara/rails'
 require 'capybara/rspec'
-#require 'capybara/poltergeist'
 
-#Capybara.javascript_driver = :poltergeist #default selenium
+Capybara.javascript_driver = :webkit #default selenium
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -25,7 +24,7 @@ RSpec.configure do |config|
   #config.mock_with :mocha
   # config.mock_with :flexmock
   # config.mock_with :rr
-  #Capybara.default_wait_time = 5
+  Capybara.default_wait_time = 10
 
   config.include FactoryGirl::Syntax::Methods
   config.include Capybara::DSL, type: :feature
