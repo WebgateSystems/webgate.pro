f0e2f46547eaa343074346e8f3e2119975ee10fa
Jerzy Sładkowski <jerzy.sladkowski@gmail.com>
2015-03-22 01:21:00 +0100

    Deployment corrections 3

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,20 +5,13 @@ set :application, 'webgate.pro'
 set :deploy_user, 'webgate'
 set :repo_url, 'git@tracker.webgate.pro:internal/webgate.git'
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
-
-# Default value for :scm is :git
 set :scm, :git
 
 # Default value for :format is :pretty
 set :format, :pretty
 
 # Default value for :log_level is :debug
-set :log_level, :debug
+set :log_level, :info
 
 set :tests, []
 
@@ -35,7 +28,7 @@ set :keep_releases, 5
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
-    invoke 'unicorn:restart'
+    invoke 'unicorn:reload'
   end
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
