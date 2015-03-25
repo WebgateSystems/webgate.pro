78c31c3344516da9d0c8561694b301ac372b2b07
Yuri Skurikhin <yunixon@gmail.com>
2015-03-25 15:50:03 +0300

    add team js specs

--- spec/features/team_spec.rb
+++ spec/features/team_spec.rb
@@ -2,18 +2,27 @@ require 'rails_helper'
 
 feature 'Adding team to site.' do
 
+  let!(:member1) { Member.create(name: 'TestName1', shortdesc: 'TestShortDesc1',
+      description: 'TestDesc1', motto: 'TestMotto1',
+      avatar: Rack::Test::UploadedFile.new(File.join(Rails.root, 'app', 'assets', 'images',  'alex_dobr.jpg'))) }
+
   before do
-    @member = create(:member)
     visit "/team"
   end
 
-  scenario 'Should created profile in list of profiles' do
-    expect(page).to have_content @member.name
-    expect(page).to have_content @member.shortdesc
+  scenario 'Should show list of team members' do
+    expect(page).to have_content member1.name
+    expect(page).to have_content member1.shortdesc
+    expect(page).to have_xpath("//img[contains(@src, \"uploads/pictures/member\")]")
   end
 
-  scenario 'List page should have avatar' do
-    expect(page).to have_xpath("//img[contains(@src, \"uploads/pictures/member\")]")
+  scenario 'Show and Hide extend team members information', js: true do
+    page.execute_script("$('span.team_name').click()")
+    expect(page).to have_content 'Technologies'
+    expect(page).to have_content 'About'
+    expect(page).to have_content member1.description
+    page.execute_script("$('span.mob.service_block_btn').click()")
+    expect(page).to_not have_content 'Technologies'
   end
 
 end
