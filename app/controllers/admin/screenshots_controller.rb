cda66fdc2b58e94267d3dabe89a07cb77c42e3ac
Yuri Skurikhin <yunixon@gmail.com>
2015-04-01 17:28:11 +0300

    multi uploader

--- app/controllers/admin/screenshots_controller.rb
+++ app/controllers/admin/screenshots_controller.rb
@@ -1,4 +1,5 @@
 class Admin::ScreenshotsController < Admin::HomeController
+  before_action :set_screenshot, only: [:destroy]
 
   def create
     @screenshot = Screenshot.new(screenshot_params)
@@ -7,20 +8,25 @@ class Admin::ScreenshotsController < Admin::HomeController
       render json: { message: 'success', fileID: @screenshot.id }, status: 200
     else
       render json: { error: @screenshot.errors.full_messages.join(',')}, status: 400
-    end 
+    end
   end
 
   def destroy
     @screenshot.destroy
     respond_to do |format|
+      format.html { redirect_to :back, notice: 'Successfully destroyed screenshot.' }
       format.json { head :no_content }
     end
   end
 
   private
-  
+
+  def set_screenshot
+    @screenshot = Screenshot.find(params[:id])
+  end
+
   def screenshot_params
-    params.require(:screenshot).permit(:file, :project_id, :position)
+    params.require(:screenshot).permit(:file, :file_cache, :project_id, :position)
   end
 
 end
