a2bc936a86d912e7d80279ce899fdae2a1f89427
Jerzy Sładkowski <jerzy.sladkowski@gmail.com>
2015-04-19 19:53:22 +0200

    Some fixes

--- app/models/technology_group.rb
+++ app/models/technology_group.rb
@@ -7,6 +7,8 @@ class TechnologyGroup < ActiveRecord::Base
   validates_presence_of :title
   validates_uniqueness_of :title, case_sensitive: false
 
+  default_scope { order(:position) }
+
   translates :title, :description
 
 end
