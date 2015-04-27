54eae5c7ef486fbf8bc0f76fc793e4456184ae5f
Yuri Skurikhin <yunixon@gmail.com>
2015-04-27 18:03:48 +0300

    contacts factory, support_mailer and spec

--- app/mailers/support_mailer.rb
+++ app/mailers/support_mailer.rb
@@ -4,6 +4,10 @@ class SupportMailer < ActionMailer::Base
   default to: 'biuro@webgate.pro'
 
   def contact_support(contact)
-    mail from: contact.email, subject: t('contact_form'), body: contact.content
+    if contact.nickname.present?
+      mail from: contact.email, subject: 'SPAM', body: contact.content
+    else
+      mail from: contact.email, subject: t('contact_form'), body: contact.content
+    end
   end
 end
--- spec/mailers/support_mailer_spec.rb
+++ spec/mailers/support_mailer_spec.rb
@@ -1,4 +1,25 @@
 require "rails_helper"
 
 describe SupportMailer, type: :mailer do
+
+  let(:contact) { build(:contact) }
+  let(:bot) { build(:bot) }
+
+  it 'has a valid factory' do
+    expect(build(:contact)).to be_valid
+  end
+
+  describe '#contact support' do
+    it 'renders the sender contact email' do
+      last_email = SupportMailer.contact_support(contact)
+      expect(last_email.from).to include contact.email
+      expect(last_email).to have_content(contact.content)
+      expect(last_email.subject).to_not include 'SPAM'
+    end
+    it 'bot contact to SPAM' do
+      last_email = SupportMailer.contact_support(bot)
+      expect(last_email.subject).to include 'SPAM'
+    end
+  end
+
 end
