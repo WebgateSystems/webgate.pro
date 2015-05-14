615319f46916df6f411f16df0258818ecf820476
Yuri Skurikhin <yunixon@gmail.com>
2015-05-14 15:29:31 +0300

    remove reset.css

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,7 +7,7 @@ Rails.application.config.assets.version = '1.1'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( ga.js admin.js contacts.js)
-Rails.application.config.assets.precompile += %w( admin.css reset.css contacts.css)
+Rails.application.config.assets.precompile += %w( admin.css contacts.css)
 %w( categories members pages projects technologies technology_groups users).each do |controller|
   Rails.application.config.assets.precompile += ["admin/#{controller}.js", "admin/#{controller}.css"]
 end
