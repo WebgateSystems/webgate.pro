a03d1e0f88e4939ccd660ba6cfdeb1346298c5a9
Yuri Skurikhin <yunixon@gmail.com>
2015-06-01 14:04:39 +0300

    rubocop: incorrect indentation

--- config/initializers/dynamic_sitemaps.rb
+++ config/initializers/dynamic_sitemaps.rb
@@ -3,8 +3,8 @@ DynamicSitemaps.configure do |config|
   config.folder = 'sitemaps' # This folder is emptied on each sitemap generation
   config.index_file_name = 'sitemap.xml'
   config.always_generate_index = false # Makes sitemap.xml contain the sitemap
-                                       # (e.g. site.xml) when only one sitemap
-                                       #  file has been generated
+  # (e.g. site.xml) when only one sitemap
+  #  file has been generated
   config.config_path = Rails.root.join('config', 'sitemap.rb')
   config.per_page = 50000
 
