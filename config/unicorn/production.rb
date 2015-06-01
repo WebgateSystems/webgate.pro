5e3c8b50f28a186491916a64186d554467a7bab9
Yuri Skurikhin <yunixon@gmail.com>
2015-06-01 15:14:55 +0300

    rubocop: deprecated methods

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -26,7 +26,7 @@ before_fork do |server, worker|
   end
 
   old_pid = "#{server.config[:pid]}.oldbin"
-  if File.exists?(old_pid) && server.pid != old_pid
+  if File.exist?(old_pid) && server.pid != old_pid
     begin
       Process.kill('QUIT', File.read(old_pid).to_i)
     rescue Errno::ENOENT, Errno::ESRCH
