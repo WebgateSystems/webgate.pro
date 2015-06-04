b3f9cdbd906e71b49fd1f72baf741f9e54047a34
Yuri Skurikhin <yunixon@gmail.com>
2015-06-04 15:45:09 +0300

    root_url in sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,7 +1,12 @@
 # Change this to your host. See the readme at https://github.com/lassebunk/dynamic_sitemaps
 # for examples of multiple hosts and folders.
+host 'webgate.pro'
+
+sitemap :root do
+  url root_url
+end
+
 ApplicationController::PUBLIC_LANGS.map(&:first).each do |l|
-  host 'webgate.pro'
   sitemap ("sitemap_#{l}").to_sym do |page|
     I18n.with_locale(l) do
       url URI.unescape(main_url), last_mod: Time.now, change_freq: 'daily', priority: 1.0
@@ -10,7 +15,7 @@ ApplicationController::PUBLIC_LANGS.map(&:first).each do |l|
       # url URI.unescape(feeds_url), last_mod: Time.now, change_freq: 'daily', priority: 1.0
 
       Page.published.with_translations(l).each do |page|
-        url root_url + page.shortlink
+        url root_url + page.shortlink, last_mod: page.updated_at, change_freq: 'daily', priority: 1.0
       end
     end
   end
