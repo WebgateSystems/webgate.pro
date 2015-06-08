e5e767271e28c8da67ce26e28ce7f982e8bc6475
Jerzy Sładkowski <jerzy.sladkowski@gmail.com>
2015-06-08 13:00:41 +0200

    bugfix for all 404 requests except html

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,6 +2,7 @@ class ErrorsController < ApplicationController
   def not_found
     respond_to do |format|
       format.html { render status: 404 }
+      format.all  { render nothing: true, status: 404 }
     end
   rescue ActionController::UnknownFormat
     render status: 404, text: t(:error_404)
