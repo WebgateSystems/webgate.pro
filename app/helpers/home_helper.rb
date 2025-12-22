1548296e9003ad8c4013514cb22b448aad2c35ee
Jerzy Sladkowski <jerzy.sladkowski@gmail.com>
2025-12-22 22:18:15 +0100

    Fix: cache

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,7 +2,16 @@ module HomeHelper
   def cache_key_for_projects
     count          = Project.count
     max_updated_at = Project.maximum(:updated_at).try(:utc).try(:to_fs, :number)
-    "projects/all-#{count}-#{max_updated_at}"
+    # Also include translations updated_at to invalidate cache when translations change
+    max_translation_updated_at = begin
+      result = ActiveRecord::Base.connection.select_one(
+        'SELECT MAX(updated_at) as max_updated_at FROM project_translations'
+      )
+      result&.dig('max_updated_at')&.to_time&.utc&.to_fs(:number)
+    rescue StandardError
+      nil
+    end
+    "projects/all-#{count}-#{max_updated_at}-t#{max_translation_updated_at}"
   end
 
   def cache_key_for_members
